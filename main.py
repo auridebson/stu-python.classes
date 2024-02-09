96752cce8c5c489f431316bdd2257159217181b7
Auridebson Lima <auridebson@vutus.com>
2024-02-09 11:48:04 -0300

    Update

---
+++ main.py
@@ -0,0 +1,18 @@
+
+class pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = nome
+
+    def setNome(nome):
+        self.nome = nome
+    
+    def setIdade(idade):
+        self.idade = idade
+
+    def getNome(self):
+        return self.nome
+    
+    def getidade(self):
+        return self.idade
+    
